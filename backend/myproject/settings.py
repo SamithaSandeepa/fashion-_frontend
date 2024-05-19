caa4b8418956911aead92f9b0777daef42b8bec4
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2024-05-19 16:16:46 +0530

    Update settings.py

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -113,14 +113,24 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'f_store',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',  # Set to the address of your database if it's not on your local machine
+#         'PORT': '5432',       # Default PostgreSQL port
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'f_store',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',  # Set to the address of your database if it's not on your local machine
-        'PORT': '5432',       # Default PostgreSQL port
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
